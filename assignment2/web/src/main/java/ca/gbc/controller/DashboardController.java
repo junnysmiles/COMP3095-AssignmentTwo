06335ef25fc3cef89132567b50368e5c1d0178fb
Nick Chinsen <nicholaschinsen@gmail.com>
2020-11-06 17:08:14 -0500

    Added future extension pages, also added bootstrap files

--- assignment2/web/src/main/java/ca/gbc/controller/DashboardController.java
+++ assignment2/web/src/main/java/ca/gbc/controller/DashboardController.java
@@ -10,4 +10,16 @@ public class DashboardController {
 
     @RequestMapping("")
     public String dashboard() { return "dashboard/index"; }
+
+    @RequestMapping("/futureExtension1")
+    public String futureExtension1() { return "dashboard/futureExtension1"; }
+
+    @RequestMapping("/futureExtension2")
+    public String futureExtension2() { return "dashboard/futureExtension2"; }
+
+    @RequestMapping("/futureExtension3")
+    public String futureExtension3() { return "dashboard/futureExtension3"; }
+
+    @RequestMapping("/futureExtension4")
+    public String futureExtension4() { return "dashboard/futureExtension4"; }
 }
