0c1d340ac31fef530218be6739a56223b54d7d22
Nick Chinsen <nicholaschinsen@gmail.com>
2020-12-03 11:59:03 -0500

    connected admin support page to database

--- assignment2/web/src/main/java/ca/gbc/controller/AdminController.java
+++ assignment2/web/src/main/java/ca/gbc/controller/AdminController.java
@@ -41,7 +41,7 @@ public class AdminController {
         return "dashboard/admin/profile";
     }
 
-    //updates admin
+    //updates admin profile
     @RequestMapping(value = "/dashboard/adminProfile", method = RequestMethod.POST)
     public String adminProfileUpdate(Model model, @Valid @ModelAttribute("user") Admin admin, Authentication authentication) {
         User updateAdmin = userRepo.findByEmail(authentication.getName());
@@ -64,23 +64,52 @@ public class AdminController {
 
     //shows a list of Clients and allows for deletion
     @RequestMapping("/dashboard/adminUsers")
-    public String listUsers(Model model, Admin admin) {
+    public String listUsers(Model model, Admin admin, Authentication authentication) {
+        User currentUser = userRepo.findByEmail(authentication.getName());
         model.addAttribute("clients", clientRepo.findAll());
-        model.addAttribute("user", admin);
+        model.addAttribute("user", currentUser);
         System.out.println(model);
         return "dashboard/admin/users";
     }
 
     @RequestMapping("/dashboard/adminInbox")
-    public String adminInbox(Model model, Admin admin) {
-        model.addAttribute("user", admin);
+    public String adminInbox(Model model, Admin admin, Authentication authentication) {
+        User currentUser = adminRepo.findByEmail(authentication.getName());
+        model.addAttribute("user", currentUser);
         return "dashboard/admin/inbox";
     }
 
     @RequestMapping("/dashboard/adminSupport")
-    public String adminSupport(Model model, Admin admin) {
+    public String adminSupport(Model model, Admin admin, Authentication authentication) {
+        User currentUser = adminRepo.findByEmail(authentication.getName());
         model.addAttribute("admins", adminRepo.findAll());
-        model.addAttribute("user", admin);
+        model.addAttribute("user", currentUser);
+        return "dashboard/admin/support";
+    }
+
+    //check if admin exists, if they do update otherwise add
+    @RequestMapping(value = "/dashboard/adminSupport", method = RequestMethod.POST)
+    public String addAdmin(Model model, @Valid @ModelAttribute("user") Admin admin, Authentication authentication) {
+        User currentUser = adminRepo.findByEmail(authentication.getName());
+        User addAdmin = adminRepo.findByEmail(admin.getEmail());
+        if(addAdmin == null) {
+            adminRepo.save(admin);
+        }else{
+            addAdmin.setFirstName(admin.getFirstName());
+            addAdmin.setLastName(admin.getLastName());
+            addAdmin.setEmail(admin.getEmail());
+            addAdmin.setDay(admin.getDay());
+            addAdmin.setMonth(admin.getMonth());
+            addAdmin.setYear(admin.getYear());
+            addAdmin.setStreetName(admin.getStreetName());
+            addAdmin.setCity(admin.getCity());
+            addAdmin.setCountry(admin.getCountry());
+            addAdmin.setPostal(admin.getPostal());
+            userRepo.save(addAdmin);
+        }
+        //add all necessary data to model
+        model.addAttribute("admins", adminRepo.findAll());
+        model.addAttribute("user", currentUser);
         return "dashboard/admin/support";
     }
 }
